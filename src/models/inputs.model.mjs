69d9f74a0df8916548642a9e879edd1971c39ab5
walteribanez555 <walteribanez555@gmail.com>
2024-09-16 14:40:21 -0400

    Model input updated

--- src/models/inputs.model.mjs
+++ src/models/inputs.model.mjs
@@ -52,7 +52,7 @@ export async function postInput({data , schema}) {
 
     if(product.length === 0){
       //Create the product if it doesn't exist and the product_id is autoincremented set the product id as product_type_id, get the product_id and set to new Register 
-      const sqlInsertProduct = `insert into products (product_id, warehouse_id, price, discount, quantity, date_created, product_type_id) values (null, 1, 0, 0, ${data.quantity}, ${actualDate.getTime()}, ${data.product_id})`;
+      const sqlInsertProduct = `insert into products (product_id, warehouse_id, price, discount, quantity, date_created, product_type_id) values (null, ${data.warehouse_id}, 0, 0, ${data.quantity}, ${actualDate.getTime()}, ${data.product_id})`;
 
       const response = await executeMysql(sqlInsertProduct, schema);
       newRegister.product_id = response.insertId;
