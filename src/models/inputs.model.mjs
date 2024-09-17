b163c4be4f56d7807d316748ceb1bbf293b375da
walteribanez555 <walteribanez555@gmail.com>
2024-09-17 11:44:21 -0400

    Model Input Updated

--- src/models/inputs.model.mjs
+++ src/models/inputs.model.mjs
@@ -8,7 +8,7 @@ const keyField = "input_id";
 const queryParams = ["id", "init", "end", "product_id", "warehouse_id"];
 
 const model = {
-  product_id: "number",
+  // product_id: "number",
   provider_id: "number",
   quantity: "number",
   detail: "string",
@@ -51,7 +51,7 @@ export async function postInput({data , schema}) {
 
     if (product.length === 0) {
       // Create the product if it doesn't exist and the product_id is autoincremented set the product id as product_type_id, get the product_id and set to new Register 
-      const sqlInsertProduct = `insert into products (product_id, warehouse_id, price, discount, quantity, date_created, product_type_id) values (null, ${data.warehouse_id}, 0, 0, ${data.quantity}, '${actualDate}', ${data.product_type_id})`;
+      const sqlInsertProduct = `insert into products (product_id, warehouse_id, price, discount, quantity, date_created, product_type_id) values (null, ${data.warehouse_id}, 0, 0, ${data.quantity}, '${actualDate}', ${data.product_id})`;
 
       const response = await executeMysql(sqlInsertProduct, schema);
       newRegister.product_id = response.insertId;
@@ -67,7 +67,7 @@ export async function postInput({data , schema}) {
     const response = await database.create(newRegister, keyField);
 
     data.date_created = actualDate;
-    data.product_id = newRegister.product_id;
+    data.provider_id = newRegister.provider_id;
 
     return buildResponse(200, response, 'post', keyField, data);
 
