4e6f20431ee1c4af2446043a564296574fc234de
walteribanez555 <walteribanez555@gmail.com>
2024-10-22 13:10:38 -0400

    Output Controller Updated

--- src/controllers/outputs.controller.mjs
+++ src/controllers/outputs.controller.mjs
@@ -1,23 +1,66 @@
-import { deleteOutput, getOutput, postOutput, putOutput } from "../models/outputs.model.mjs";
+import { putInput } from "../models/inputs.model.mjs";
+import {
+  deleteOutput,
+  getOutput,
+  postOutput,
+  putOutput,
+} from "../models/outputs.model.mjs";
+import { buildResponse } from "../utils/helpers.mjs";
 
+export async function getOutputs({
+  id,
+  init,
+  end,
+  product_id,
+  warehouse_id,
+  limit,
+  offset,
+  product_type_id,
+}) {
+  // return getOutput({id,init, end, product_id, warehouse_id, limit , offset, product_type_id, schema : 'monster'});
 
+  const [err, response] = await getOutput({
+    id,
+    init,
+    end,
+    product_id,
+    warehouse_id,
+    limit,
+    offset,
+    product_type_id,
+    schema: "monster",
+  });
 
-export async function getOutputs({ id,init, end,product_id,warehouse_id, limit, offset, product_type_id }) {
-    return getOutput({id,init, end, product_id, warehouse_id, limit , offset, product_type_id, schema : 'monster'}); 
+  if (err) return buildResponse(err.status, err.message, "get");
 
+  return buildResponse(200, response, "get");
 }
 
 export async function postOutputs({ data }) {
-    return postOutput({data, schema : 'monster'});
+  //   return postOutput({ data, schema: "monster" });
+  const [err, { queryResponse, keyField, dataResponse }] = await postOutput({
+    data,
+    schema: "monster",
+  });
 
+  if (err) return buildResponse(err.status, err.message, "post");
+
+  return buildResponse(200, queryResponse, "post", keyField, dataResponse);
 }
 
-export async function putOutputs({ id, data}){
-    return putOutput({ id, data , schema : 'monster'});
+export async function putOutputs({ id, data }) {
+  //   return putOutput({ id, data, schema: "monster" });
+  const [err, result] = await putInput({ id, data, schema: "monster" });
 
+  if (err) return buildResponse(err.status, err.message, "put");
+
+  return buildResponse(200, result, "put");
 }
 
-export async function deleteOutputs({ id }){
-    return deleteOutput({id , schema : 'monster'})
-    
+export async function deleteOutputs({ id }) {
+  //   return deleteOutput({ id, schema: "monster" });
+  const [err, result] = await deleteOutput({ id, schema: "monster" });
+
+  if (err) return buildResponse(err.status, err.message, "delete");
+  return buildResponse(200, result, "delete");
 }
--- src/models/outputs.model.mjs
+++ src/models/outputs.model.mjs
@@ -5,14 +5,33 @@ const tableName = "outputs";
 const idField = "output_id";
 const keyField = "output_id";
 
-const queryParams = ["id", "init", "end", "product_id", "warehouse_id", "product_type_id"];
+const queryParams = [
+  "id",
+  "init",
+  "end",
+  "product_id",
+  "warehouse_id",
+  "product_type_id",
+];
 
 const model = {
   quantity: "number",
   detail: "string",
 };
 
-export async function getOutput({ id, schema, init, end, product_id, warehouse_id, limit, offset, product_type_id }) {
+export async function getOutput({
+  id,
+  schema,
+  init,
+  end,
+  product_id,
+  warehouse_id,
+  limit,
+  offset,
+  product_type_id,
+}) {
+  let response;
+
   try {
     const database = new DatabaseOperations("OutputProductDetails", schema);
     const data = {
@@ -20,7 +39,7 @@ export async function getOutput({ id, schema, init, end, product_id, warehouse_i
         [keyField]: id,
         [queryParams[3]]: product_id,
         [queryParams[4]]: warehouse_id,
-        [queryParams[5]]: product_type_id
+        [queryParams[5]]: product_type_id,
       },
       init,
       end,
@@ -28,15 +47,18 @@ export async function getOutput({ id, schema, init, end, product_id, warehouse_i
       offset,
     };
 
-    const response = await database.read(data);
-    return buildResponse(200, response, "get");
+    const queryReponse = await database.read(data);
+    response = [undefined, queryReponse];
   } catch (error) {
     colorLog(`Get Output error : ${JSON.stringify(error)} `, "red", "reset");
-    return buildResponse(500, error, "get");
+    // return buildResponse(500, error, "get");
+    response = [{ status: 500, message: error }, undefined];
   }
+  return response;
 }
 
 export async function postOutput({ data, schema }) {
+  let response;
   try {
     const database = new DatabaseOperations(tableName, schema);
     const newRegister = validateData(data, model);
@@ -45,37 +67,49 @@ export async function postOutput({ data, schema }) {
     const product = await executeMysql(sql, schema);
 
     if (product.length === 0) {
-      return buildResponse(400, { message: "Product not found" }, "post");
+      // return buildResponse(400, { message: "Product not found" }, "post");
+      response = [{ status: 400, message: "Product not found" }, undefined];
+      return response;
     }
 
     if (product[0].quantity < data.quantity) {
-      return buildResponse(400, { message: "Not enough quantity in stock" }, "post");
+      // return buildResponse(400, { message: "Not enough quantity in stock" }, "post");
+      response = [
+        { status: 400, message: "Missing the record id to Update" },
+        undefined,
+      ];
+      return response;
     }
 
-    const sqlUpdate = `update products set quantity = ${product[0].quantity - data.quantity} where product_id = ${product[0].product_id}`;
+    const sqlUpdate = `update products set quantity = ${
+      product[0].quantity - data.quantity
+    } where product_id = ${product[0].product_id}`;
     await executeMysql(sqlUpdate, schema);
 
     newRegister.product_id = product[0].product_id;
     data.product_id = product[0].product_id;
-    data.product_type_id  = product[0].product_type_id;
+    data.product_type_id = product[0].product_type_id;
 
     const actualDate = new Date().toISOString();
     newRegister.date_created = actualDate;
 
-
     data.date_created = actualDate;
 
-    const response = await database.create(newRegister, keyField);
+    const queryResponse = await database.create(newRegister, keyField);
 
-    return buildResponse(200, response, "post", keyField, data);
-   
+    response = [undefined, { queryResponse, keyField, dataResponse: data }];
+
+    // return buildResponse(200, response, "post", keyField, data);
   } catch (err) {
     colorLog(`POST SERVICES ERROR : ${JSON.stringify(err)}`, "red", "reset");
-    return buildResponse(500, err, "post");
+    // return buildResponse(500, err, "post");
+    response = [{ status: 500, message: err }, undefined];
   }
 }
 
 export async function putOutput({ id, data, schema }) {
+  let response;
+
   try {
     const database = new DatabaseOperations(tableName, schema);
     const update = validateData(data, model, "put");
@@ -83,36 +117,54 @@ export async function putOutput({ id, data, schema }) {
     if (Object.keys(update).length === 0)
       return buildResponse(400, { message: "Missing fields to update" }, "put");
 
-    if (!id)
-      return buildResponse(
-        400,
-        { message: "Miising the record id to update" },
-        "put"
-      );
+    if (!id) {
+      response = [
+        { status: 400, message: "Missing the record id to update" },
+        undefined,
+      ];
+
+      return response;
+    }
+    // return buildResponse(
+    //   400,
+    //   { message: "Miising the record id to update" },
+    //   "put"
+    // );
 
     const where = {
       [keyField]: id,
     };
 
-    const response = await database.update(update, where);
-    return buildResponse(200, response, "put");
+    const queryResponse = await database.update(update, where);
+    // return buildResponse(200, response, "put");
+    response = [undefined, queryResponse];
+
+
   } catch (err) {
     colorLog(`Put Output error : ${JSON.stringify(err)}`, "red", "reset");
-    return buildResponse(500, err, "put");
+    // return buildResponse(500, err, "put");
+    response = [{status: 500, message: err}, undefined];
   }
+  return response;
 }
 
 export async function deleteOutput({ id, schema }) {
+  let response;
   try {
     const database = new DatabaseOperations(tableName, schema);
 
-    if (!id)
-      return buildResponse(400, { message: "Missing the record id to delete" });
+    if (!id){
+      response = [{status:400, message: 'Missing the record id to delete'}, undefined];
+      return response;
+    }
+      // return buildResponse(400, { message: "Missing the record id to delete" });
     await database.delete(id, keyField);
-    return buildResponse(200, { message: "Record deleted" }, "delete");
+    response = [undefined, "Record deleted"];
+    // return buildResponse(200, { message: "Record deleted" }, "delete");
   } catch (err) {
     colorLog(`Delete Output error : ${JSON.stringify(err)}`, "red", "reset");
 
-    return buildResponse(500, err, "delete");
+    response = [{status:500, message : err}, undefined];
+    // return buildResponse(500, err, "delete");
   }
 }
